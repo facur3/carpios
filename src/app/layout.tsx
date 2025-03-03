b60617ec7be746599a746ac359086ff0e48a9f6c
facur <facurossi33@gmail.com>
2025-03-03 19:15:42 -0300

    navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,14 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Lama Dev E-Commerce Application",
-  description: "A complete e-commerce application with Next.js and Wix",
+  title: "Carpios - Venta de pines y accesorios",
+  description: "Una colección de pines y accesorios para ti",
 };
 
 export default function RootLayout({
@@ -16,7 +18,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+        <Footer />
+        </body>
     </html>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,30 @@
+import Link from "next/link"
+import Menu from "./Menu"
+
 const Navbar = () => {
     return (
-        <div className=''>Navbar</div>
-    )
-}
+        <div className='h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64'>
+            {/* MOBILE */}
+            <div className='flex items-center justify-between'>
+                <Link href='/'>
+                    <div className="text-2xl tracking-wide">Carpios</div>
+                </Link>
+            <Menu/>
+            </div>
+            {/* BIGGER SCREENS */}
+            <div className="hidden md:flex items-center justify-between gap-8 h-full">
+                {/* LEFT */}
+                <div className="w-1/3">
+                <Link href='/'>
+                <div className="text-2xl tracking-wide">Carpios</div>
+                </Link>
+                </div>
+                {/* RIGHT */}
+                <div className="w-2/3"></div>
+            </div>
+        </div>
+    );
+};
 
-export default Navbar
+export default Navbar;
+
