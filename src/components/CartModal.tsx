48553c1a8adb5424f0868d27213ef2e9da68c615
facur <facurossi33@gmail.com>
2025-03-03 21:07:54 -0300

    cart modal update spans

--- src/components/CartModal.tsx
+++ src/components/CartModal.tsx
@@ -9,26 +9,30 @@ const CartModal = () => {
 
 
     return (
-        <div className='absolute p-4 rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] bg-white top-12 right-0 flex flex-col gap-6 z-20'>
+        <div className='w-max absolute p-4 rounded-md shadow-[0_3px_10px_rgb(0,0,0,0.2)] bg-white top-12 right-0 flex flex-col gap-6 z-20'>
             {!cartItems ? (
                 <div className="">Cart is empty</div>
             ) : (
-                <div className="">
+                <div className="flex gap-4">
                     <Image src='https://images.pexels.com/photos/30649019/pexels-photo-30649019/free-photo-of-helado-de-vainilla-servido-en-vaso-sobre-fondo-rojo-brillante.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load' alt='' width={72} height={96} className="object-cover rounded-md"/>
-                    <div className="">
+                    <div className="flex flex-col justify-between w-full">
                         {/* TOP */}
                         <div className="">
                         {/* TITLE */}
-                        <div className="">
-                            <h3>Product Name</h3>
-                            <div className="">$49</div>
+                        <div className="flex items-center justify-between gap-8">
+                            <h3 className="font-semibold">Product Name</h3>
+                            <div className="p-1 bg-gray-50 rounded-md">$49</div>
                         </div>
                         {/* DESC */}
-                        <div className="">
+                        <div className="text-sm text-gray-500">
                             available
                         </div>
                         </div>
                         {/* BOTTOM */}
+                        <div className="flex justify-between text-sm">
+                            <span className="text-gray-500">Qty. 2</span>
+                            <span className="text-blue-500">Remove</span>
+                        </div>
                     </div>
                 </div>
             )}
