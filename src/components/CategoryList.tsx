a00a42387221616d54f1c59190899dd816dc6230
facur3 <facurossi33@gmail.com>
2025-03-04 15:59:55 -0300

    add footer, and categories

--- src/components/CategoryList.tsx
+++ src/components/CategoryList.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 
 const CategoryList = () => {
     return (
-        <div className='px-4 overflow-x-scroll'>
+        <div className='px-4 overflow-x-scroll scrollbar-hide'>
             <div className="flex gap-4 md:gap-8">
                 <Link href='/list?cat=test' className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6">
                     <div className="relative bg-slate-100 w-full h-96">
